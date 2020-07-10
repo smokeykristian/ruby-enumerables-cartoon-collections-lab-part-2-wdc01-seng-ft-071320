939daa2d518fa81484182e115bb1decad9934f6f
Kristian Portillo <portillk@gmail.com>
2020-07-10 08:14:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
  num = 0 
-valid_calls[num].find {planeteer_calls}
+planeteer_calls.find {valid_calls[num]}
 num += 1
 
   
